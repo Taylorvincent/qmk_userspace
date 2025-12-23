0b6d3d2dd4616a019d4bcf3a94c2391d3c5af89d
Vincent Taylor <vincent@wegroup.be>
2025-12-23 17:25:35 +0100

    use flow tap

--- keyboards/crkbd/keymaps/vincent/config.h
+++ keyboards/crkbd/keymaps/vincent/config.h
@@ -1,16 +1,11 @@
 #pragma once
 
-// SM_TD Configuration
-// Timeout values in milliseconds
-#define SMTD_GLOBAL_TAP_TERM 200
-#define SMTD_GLOBAL_FOLLOWING_TAP_TERM 250
-#define SMTD_GLOBAL_SEQUENCE_TERM 100
-#define SMTD_GLOBAL_RELEASE_TERM 50
-
-
+// #define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
+#define FLOW_TAP_TERM 150
 #define COMBO_COUNT 105
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 250
 #define COMBO_TERM 35
 
-#define ONESHOT_TIMEOUT 5000
+#define ONESHOT_TIMEOUT 4000
