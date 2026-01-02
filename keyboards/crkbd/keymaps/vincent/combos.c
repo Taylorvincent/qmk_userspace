497d169c7a3ac969b038bc937fc68b39a4c87e8a
Vincent Taylor <vincent@wegroup.be>
2026-01-02 23:08:52 +0100

    comobs

--- keyboards/crkbd/keymaps/vincent/combos.c
+++ keyboards/crkbd/keymaps/vincent/combos.c
@@ -1,4 +1,14 @@
-const uint16_t PROGMEM test_combo1[] = {KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM dw_combo[] = {KC_D, KC_W, COMBO_END};
+const uint16_t PROGMEM ld_combo[] = {KC_L, KC_D, COMBO_END};
+const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM ha_combo[] = {KC_H, KC_A, COMBO_END};
+const uint16_t PROGMEM ae_combo[] = {KC_A, KC_E, COMBO_END};
+// const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I, COMBO_END};
 combo_t key_combos[] = {
-    COMBO(test_combo1, KC_ESC),
+    COMBO(dw_combo, KC_ESC),
+    COMBO(ld_combo, KC_ESC),
+    COMBO(rt_combo, KC_TAB),
+    COMBO(ha_combo, KC_BSPC),
+    COMBO(ae_combo, KC_DEL),
+    // COMBO(ei_combo, KC_BSPC),
 };
