4eb23830a95f7d6101dd4e7621befa12e9b02a8e
Vincent Taylor <vincent@wegroup.be>
2026-01-04 21:53:53 +0100

    tweaks

--- keyboards/crkbd/keymaps/vincent/combos.c
+++ keyboards/crkbd/keymaps/vincent/combos.c
@@ -1,9 +1,18 @@
-const uint16_t PROGMEM dw_combo[] = {KC_D, KC_W, COMBO_END};
+/**
+ b l d w q   j f o u ' 
+ n r t s g   y h a e i 
+ z x m c v   k p , . /    
+*/
+
 const uint16_t PROGMEM ld_combo[] = {KC_L, KC_D, COMBO_END};
-const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM ha_combo[] = {KC_H, KC_A, COMBO_END};
-const uint16_t PROGMEM ae_combo[] = {KC_A, KC_E, COMBO_END};
-// const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I, COMBO_END};
+const uint16_t PROGMEM dw_combo[] = {KC_D, KC_W, COMBO_END};
+const uint16_t PROGMEM xm_combo[] = {KC_X, KC_M, COMBO_END};
+const uint16_t PROGMEM mc_combo[] = {KC_M, KC_C, COMBO_END};
+const uint16_t PROGMEM fo_combo[] = {KC_F, KC_O, COMBO_END};
+const uint16_t PROGMEM ou_combo[] = {KC_O, KC_U, COMBO_END};
+const uint16_t PROGMEM p_comma_combo[] = {KC_P, KC_COMMA, COMBO_END};
+const uint16_t PROGMEM comma_period_combo[] = {KC_COMMA, KC_PERIOD, COMBO_END};
+
 const uint16_t PROGMEM gy_combo[] = {KC_G, KC_Y, COMBO_END};
 combo_t key_combos[] = {
     COMBO(dw_combo, KC_ESC),
@@ -11,6 +20,10 @@ combo_t key_combos[] = {
     COMBO(rt_combo, KC_TAB),
     COMBO(ha_combo, KC_BSPC),
     COMBO(ae_combo, KC_DEL),
-    // COMBO(ei_combo, KC_BSPC),
+    combo(fo_combo, KC_BSPC),
+    combo(ou_combo, KC_BSPC),
+    combo(p_comma_combo, KC_DEL),
+    combo(comma_period_combo, KC_ESC),
+
     COMBO(gy_combo, CW_TOGG),
 };
